96de599affa9326555c9719b9c1153052006bc7a
Matej Pavic <matej@Matejs-MacBook-Pro.local>
2023-07-28 20:00:05 +0200

    Add jquery example

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-(async function() {
-    const source = document.getElementById("entry-template").innerHTML;
+$(document).ready(async function() {
+  try {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon-color/yellow"
+    );
+    const data = await response.json();
+    const source = $("#pokemon-list").html();
     const template = Handlebars.compile(source);
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon-color/yellow")
-    const data = await response.json()
-    const pokemons = data.pokemon_species.slice(0, 20)
-    const context = { pokemons };
+    const context = {
+      pokemon: data.pokemon_species.slice(0, 20),
+      title: "Pokemons",
+    };
     const html = template(context);
-    
-    document.querySelector("body").innerHTML = html
-})()
+
+    $("#result").html(html);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
