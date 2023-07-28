a30b5b370fc53670e86d3058fb93dae8e210d440
Matej Pavic <matej@Matejs-MacBook-Pro.local>
2023-07-28 19:27:04 +0200

    Change code for fetching data example

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const source = document.getElementById("entry-template").innerHTML;
-const template = Handlebars.compile(source);
-
-const context = { title: "My New Post", body: "This is my first post!", imgURL:"https://robohash.org/matej" };
-const html = template(context);
-
-console.log(html)
-
-document.querySelector("body").innerHTML = html
+(async function() {
+    const source = document.getElementById("entry-template").innerHTML;
+    const template = Handlebars.compile(source);
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon-color/yellow")
+    const data = await response.json()
+    const pokemons = data.pokemon_species.slice(0, 20)
+    const context = { pokemons };
+    const html = template(context);
+    
+    document.querySelector("body").innerHTML = html
+})()
