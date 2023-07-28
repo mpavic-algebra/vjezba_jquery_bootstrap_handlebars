3f3329c81194d6231d4d9e59d6c497baa9c1c36e
Matej Pavic <matej@Matejs-MacBook-Pro.local>
2023-07-28 20:28:52 +0200

    Add more Jquery

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-$(document).ready(async function() {
-  try {
-    const response = await fetch(
-      "https://pokeapi.co/api/v2/pokemon-color/yellow"
-    );
-    const data = await response.json();
+
+$(document).ready(function() {
+  $.get("https://pokeapi.co/api/v2/pokemon-color/yellow")
+    .done(render)
+    .fail(function(error) {
+      console.log(error);
+  });
+
+  $("#green").click(function() {
+    $.get("https://pokeapi.co/api/v2/pokemon-color/green")
+    .done(render)
+    .fail(function(error) {
+      console.log(error);
+    });
+  })
+
+  $("#yellow").click(function() {
+    $.get("https://pokeapi.co/api/v2/pokemon-color/yellow")
+    .done(render)
+    .fail(function(error) {
+      console.log(error);
+    });
+  })
+
+  $("#blue").click(function() {
+    $.get("https://pokeapi.co/api/v2/pokemon-color/blue")
+    .done(render)
+    .fail(function(error) {
+      console.log(error);
+    });
+  })
+
+  function render(data) {
     const source = $("#pokemon-list").html();
-    const template = Handlebars.compile(source);
-    const context = {
-      pokemon: data.pokemon_species.slice(0, 20),
-      title: "Pokemons",
-    };
-    const html = template(context);
+      const template = Handlebars.compile(source);
+      const context = {
+        pokemon: data.pokemon_species.slice(0, 20),
+        title: "Pokemons",
+      };
+      const html = template(context);
 
-    $("#result").html(html);
-  } catch (error) {
-    console.log(error);
+      $("#result").html(html);
   }
-});
 
+})
